90437a4a7a327b5bfc531383e6f25ed9556d0271
AndreasStapnes <projstapnes@gmail.com>
2022-03-28 13:50:07 +0200

    Increased usability

--- main.py
+++ main.py
@@ -16,6 +16,22 @@ def cross(vec1: Vec, vec2: Vec):
         result[i] = np.linalg.det(det_obj) * (-1)**i
     return result
 
+class Vec2d(np.ndarray):
+    def __new__(cls, values: Optional[Tuple[float, float]], **kwargs):
+        obj = super().__new__(cls, shape=(2,), **kwargs)
+        obj.__init__(values, **kwargs)
+        return obj
+    def __init__(self, values, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(2):
+            self.__setitem__(i, values[i])
+    def magnitude(self):
+        return self.abs()
+    def dir(self):
+        return self / self.magnitude()
+    def __abs__(self):
+        return np.linalg.norm(self)
+
 
 class Vec(np.ndarray):
     def __new__(cls, values: Optional[Tuple[float, float, float]], **kwargs):
@@ -49,6 +65,7 @@ class Dipole(Vec):
         elif hasattr(position, '__iter__'):
             return np.array([mu0/(4*np.pi)*self/abs(r) for r in position])
         else:
+
             raise Exception("An error has occurred")
 
     def B(self, position):
@@ -56,12 +73,28 @@ class Dipole(Vec):
             r = Vec(position)
             r_siz = abs(r)
             return mu0/(4*np.pi*r_siz*r_siz*r_siz) * (3*r.dir()*np.dot(r.dir(), self) - self)
-        elif hasattr(position, '__iter__'):
-            rs = np.array(position)
-            sizes = np.apply_along_axis(np.linalg.norm, 1, rs)
-            dirs = rs/sizes[:, np.newaxis]
-            return mu0/(4*np.pi*(sizes**3)[:,np.newaxis])*(3*dirs*(dirs @ self)[:,np.newaxis] - self)
 
+        elif isinstance(position, np.ndarray) and np.shape(position)[-1] == 3:
+            initial_shape = np.shape(position)
+            flat_position = np.reshape(position, (-1,3))
+            B_flattened = self.__B_flattened__(flat_position)
+            return np.reshape(B_flattened, initial_shape)
+
+        elif hasattr(position, '__iter__'):
+            return self.__B_flattened__(position)
+
+        else:
+            raise Exception("positions were of invalid type")
+
+    def __B_flattened__(self, position):
+        assert hasattr(position, '__iter__')
+        rs = np.array(position)
+        sizes = np.apply_along_axis(np.linalg.norm, 1, rs)
+        dirs = rs / sizes[:, np.newaxis]
+        return mu0 / (4 * np.pi * (sizes ** 3)[:, np.newaxis]) * (3 * dirs * (dirs @ self)[:, np.newaxis] - self)
+
+def project_along(vector: Vec, along: Vec) -> Vec2d:
+    pass
 
 m = Dipole((0,0,1))
 xs, ys = np.meshgrid(np.linspace(-1,1,60), np.linspace(-1,1,60))
@@ -77,7 +110,7 @@ rs_sizes = np.apply_along_axis(np.linalg.norm, 2, rs)
 
 import matplotlib.pyplot as plt
 shape = np.shape(spatial_xs)
-B_vals = np.reshape(m.B(rs.reshape((-1,3))), shape)
+B_vals = m.B(rs)
 B_vals = np.apply_along_axis(np.linalg.norm, 2, B_vals)
 B_vals = np.where(rs_sizes > 0.1, np.log(B_vals), -15)
 
